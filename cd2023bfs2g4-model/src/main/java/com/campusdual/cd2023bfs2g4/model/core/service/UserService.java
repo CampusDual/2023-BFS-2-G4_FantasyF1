d7a10f8f86db99a678e84e08ebe9f711220737e0
Pablo Martínez Kirsten <pablo.martinez@imatia.com>
2024-02-20 09:33:32 +0100

    Backend

---
+++ cd2023bfs2g4-model/src/main/java/com/campusdual/cd2023bfs2g4/model/core/service/UserService.java
@@ -0,0 +1,56 @@
+package com.campusdual.cd2023bfs2g4.model.core.service;
+
+import com.campusdual.cd2023bfs2g4.api.core.service.IUserService;
+import com.campusdual.cd2023bfs2g4.model.core.dao.UserDao;
+import com.ontimize.jee.common.dto.EntityResult;
+import com.ontimize.jee.server.dao.DefaultOntimizeDaoHelper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Map;
+
+@Lazy
+@Service("UserService")
+public class UserService implements IUserService {
+
+	@Autowired
+	private UserDao userDao;
+
+	@Autowired
+	private DefaultOntimizeDaoHelper daoHelper;
+
+	public void loginQuery(Map<?, ?> key, List<?> attr) {
+	}
+
+	//Sample to permission
+	//@Secured({ PermissionsProviderSecured.SECURED })
+	public EntityResult userQuery(Map<?, ?> keyMap, List<?> attrList) {
+		return this.daoHelper.query(userDao, keyMap, attrList);
+	}
+
+	@Override
+	public EntityResult userEmailQuery(Map<?, ?> keyMap, List<?> attrList) {
+		return this.daoHelper.query(userDao, keyMap, attrList, "emailRegistered");
+	}
+
+	public EntityResult userInsert(Map<?, ?> attrMap) {
+		return this.daoHelper.insert(userDao, attrMap);
+	}
+	@Override
+	public EntityResult userRegisterInsert(Map<?, ?> attrMap) {
+		return this.daoHelper.insert(userDao, attrMap);
+	}
+
+
+	public EntityResult userUpdate(Map<?, ?> attrMap, Map<?, ?> keyMap) {
+		return this.daoHelper.update(userDao, attrMap, keyMap);
+	}
+
+	public EntityResult userDelete(Map<?, ?> keyMap) {
+		return this.daoHelper.delete(this.userDao, keyMap);
+	}
+
+
+}
